f4756a402eb4cc727158298c795fb6ab6360160f
Eric C Schoenborn <cultureondemand@gmail.com>
2019-03-27 19:43:37 -0400

    Update content.js

--- assets/js/content.js
+++ assets/js/content.js
@@ -2,15 +2,23 @@ var slideData = [
 
   
   {
-    section: "The National Mall. Washington, DC",
+    section: "",
 //////////////////////////     icon: "pcy.svg",
     header: "",
-    video: "yes",
+ /*  video: "yes",
    bkgImage: {
      wm: "assets/images/bkg/catharsis.webm",
        mp: "assets/images/bkg/catharsis.mp4",
       name:"1"
+    },*/
+    
+        bkgImage: {
+      small: "grimeblue.png",
+      medium: "grimeblue.png",
+      large: "grimeblue.png"
+      
     },
+
     body: `
 <strong>Catharsis on the Mall</strong><br />JavaScript, WebGL, HTML5, Video, WebRTC, Projection Mapping<br />90' x 20'<br />November 9-11, 2018<br />National Mall<br /><br /> Developed Javascript browser app in Three.js to drive installation created with 3 projectors hung from the frame of a dome constructed in the center of the National Mall. Mapped the app using MadMapper.  
     `
@@ -20,16 +28,19 @@ var slideData = [
   
 
   {
-    section: "K Street. Washington, DC ",
+    section: " ",
 //////////////////     icon: "pcy.svg",
     header: "",
  //////////////////////////   video: "yes",
-    bkgImage: {
-      small: "drcbf_insta.jpg",
-      medium: "projecitonsdc.jpg",
-      large: "projecitonsdc.jpg"
+ 
+    
+        bkgImage: {
+      small: "grimeblue.png",
+      medium: "grimeblue.png",
+      large: "grimeblue.png"
       
-    },
+    },    
+    
     body: `
 <strong>Guerilla Kavanaugh Projections</strong><br />Videography, Art Direction, Projection Mapping<br />42' x 42'<br />October 5, 2018<br />Washington, DC<br /><br /> Outdoor mobile projection mapping on landmarks along the power center of DC during the Brett Kavanaugh confirmation hearings.  
     `
@@ -39,14 +50,21 @@ var slideData = [
   
   
   {
-    section: "Eaton House. Washington, DC",
+    section: "",
 //////////////////////////     icon: "pcy.svg",
     header: "",
-    video: "yes",
+  /*  video: "yes",
    bkgImage: {
      wm: "assets/images/bkg/moyaone.webm",
        mp: "assets/images/bkg/moyaone.mp4",
       name:"2"
+    },*/
+    
+        bkgImage: {
+      small: "grimeblue.png",
+      medium: "grimeblue.png",
+      large: "grimeblue.png"
+      
     },
     body: `
 <strong>Piscataway 360</strong><br />Videography, Field Audio, JavaScript, WebGL, HTML5, WebRTC, Projection Mapping<br />86' x 14'<br />September 28-30, 2018<br />Human Progress Festival<br /><br />Immersive storytelling installation created in collaboration with Sebi Medina-Tayac to celebrate the Piscataway Indian Nation of the Washington, DC area. I shot video and captured field audio from ancestral Piscataway land, designed a storyworld in JavaScript and WebGL, mounted 3 projectors and mapped the experience in MadMapper. 
@@ -61,13 +79,15 @@ var slideData = [
 //////////////////     icon: "pcy.svg",
     header: "",
  //////////////////////////   video: "yes",
-    bkgImage: {
-      small: "2018_2018__Rob_Kerr.23.jpg",
-      medium: "2018_2018__Rob_Kerr.23.jpg",
-      large: "2018_2018__Rob_Kerr.23.jpg"
+ 
+    
+        bkgImage: {
+      small: "grimeblue.png",
+      medium: "grimeblue.png",
+      large: "grimeblue.png"
       
     },
-    body: `
+  body: `
 <strong>Pickathon Cosmos</strong><br />Curation, JavaScript, WebGL, HTML5, WebRTC, Projection Mapping<br />196' x 132'<br />August 3-5, 2018<br />Pickathon Music Festival<br /><br /> Co-curated digital art program and led the setup and execution of projection mapping original content within a custom designed venue at this innovative Pacific Northwest music festival. 
     `
   },
@@ -78,12 +98,16 @@ var slideData = [
     section: "The Groves Homestead. Brethren, MI",
 //////////////////////////     icon: "pcy.svg",
     header: "",
-    bkgImage: {
-      small: "barnface.png",
-      medium: "barnface.png",
-      large: "barnface.png"
+ 
+    
+        bkgImage: {
+      small: "grimeblue.png",
+      medium: "grimeblue.png",
+      large: "grimeblue.png"
+      
     },
-    body: `
+  
+  body: `
 <strong>Augmented Americana</strong><br />JavaScript, WebGL, HTML5, WebRTC, Projection Mapping<br />96' x 52'<br />June 7, 2018<br />Brethren, MI<br /><br />Long exposure photo of solo, off-the-grid projection mapping of audio and video responsive web app mapped onto a backwoods barn in Northern Michigan.   
      `
   },
@@ -97,13 +121,17 @@ var slideData = [
     section: "New World Center for the Arts. Miami Beach, FL",
 //////////////////////////     icon: "pcy.svg",
     header: "",
-    video: "yes",
-   bkgImage: {
-     wm: "assets/images/bkg/idc.webm",
-       mp: "assets/images/bkg/idc.mp4",
-      name:"3"
+ /////   video: "yes",
+ 
+    
+        bkgImage: {
+      small: "grimeblue.png",
+      medium: "grimeblue.png",
+      large: "grimeblue.png"
+      
     },
-    body: `
+  
+  body: `
 <strong>I Don't Care if I Do Die Do</strong><br />Video, JavaScript, WebGL, WebRTC, Projection Mapping<br />36' x 12'<br />March 13-18, 2017<br />Borscht Film Festival<br /><br />Clip from transmedia documentary created with support from Borscht Film Festival. I wrote, directed and edited the documentary and filmed with a small crew of Miami filmmakers supporting me during a 1 week shoot on Loop Road in Big Cypress Swamp. Released as projection mapping installation at Borsch Film Festival 2017. 
     `
   },
@@ -115,11 +143,16 @@ var slideData = [
     section: "New World Center for the Arts. Miami Beach, FL",
 //////////////////////////     icon: "pcy.svg",
     header: "",
-    bkgImage: {
-      small: "borscht.jpeg",
-      medium: "borscht.jpeg",
-      large: "borscht.jpeg"
+ 
+    
+        bkgImage: {
+      small: "grimeblue.png",
+      medium: "grimeblue.png",
+      large: "grimeblue.png"
+      
     },
+  
+  
     body: `
 <strong>New World Architecture Mapping</strong><br />Videography, Projection Mapping<br />Architecture Mapping<br />March 13-18, 2017<br />Borscht Film Festival<br /><br />Architecture mapping of the interior of Frank Gehry designed New World Center building on Miami Beach, for takeover of the space by innovative Miami film festival. Led setup and positioning of 9 projectors throughout the interior of the space and mapped in MadMapper.    
     `
@@ -132,11 +165,18 @@ var slideData = [
   {
     section: "Parse Seco Gallery. Taos, NM",
 header: "",
-    video: "yes",
+ /*   video: "yes",
    bkgImage: {
      wm: "assets/images/bkg/wbwb.webm",
        mp: "assets/images/bkg/wbwb.mp4",
       name:"4"
+    },*/
+    
+        bkgImage: {
+      small: "grimeblue.png",
+      medium: "grimeblue.png",
+      large: "grimeblue.png"
+      
     },
 
     body:`
@@ -156,13 +196,20 @@ header: "",
   {
     section: "Parse Seco Gallery. Taos, NM",
 header: "",
-    video: "yes",
+/*    video: "yes",
    bkgImage: {
      wm: "assets/images/bkg/wb.webm",
        mp: "assets/images/bkg/wb.mp4",
       name:"5"
     },
-
+*/
+    
+        bkgImage: {
+      small: "grimeblue.png",
+      medium: "grimeblue.png",
+      large: "grimeblue.png"
+      
+    },
     body:`
  <strong>Wild Beast App</strong><br />Video, Photos, JavaScript, WebGL, WebRTC, HTML5<br />September 1, 2016<br />Web Based Project<br /><br />Browser-based immersive storytelling adventure about backroads road trip with a Joel Meinholz. We collaborated on capturing field audio, photos and video from sacred and abandoned sites in the American West. I designed and developed this browser app in JavaScript, WebRTC and HTML5.
  `
@@ -179,13 +226,16 @@ header: "",
 //////////////////     icon: "pcy.svg",
     header: "",
  //////////////////////////   video: "yes",
-    bkgImage: {
-      small: "skate_1.jpg",
-      medium: "skate_1.jpg",
-      large: "skate_1.jpg"
+ 
+    
+        bkgImage: {
+      small: "grimeblue.png",
+      medium: "grimeblue.png",
+      large: "grimeblue.png"
       
     },
-    body: `
+  
+  body: `
  <strong>Philly Radness</strong><br />Skatepark Design, JavaScript, WebGL, HTML5, WebRTC, Projection Mapping<br />96' x 12'<br />March 1 - May 1, 2016<br />Drexel University<br /><br />Adapted an interactive skatepark concept for a Philadelphia specific show around the closing of iconic skate spot, Love Park. Designed the park, mounted 6 projectors, hung 2 cameras with mics and built the setup in collaboration with Ed Selego. Designed and developed the sound and motion reactive browser app in JavaScript, WebRTC and HTML5.
     `
   },
@@ -196,13 +246,20 @@ header: "",
   {
     section: "Winter Music Conference. Miami, FL",
 header: "",
-    video: "yes",
+  /*  video: "yes",
    bkgImage: {
      wm: "assets/images/bkg/fordistas.webm",
        mp: "assets/images/bkg/fordistas.mp4",
       name:"6"
     },
-
+*/
+    
+        bkgImage: {
+      small: "grimeblue.png",
+      medium: "grimeblue.png",
+      large: "grimeblue.png"
+      
+    },
     body:`
  <strong>Fordistas: Infiltrate 17.0</strong><br />JavaScript, WebGL, Projection Mapping, VR<br />Web Based Project<br />March 13, 2016<br />Product/81<br /><br />Sound-reactive projection and mobile VR experience designed for this long-running Miami Music Conference alternative event.
 
@@ -219,12 +276,14 @@ header: "",
 //////////////////     icon: "pcy.svg",
     header: "",
  //////////////////////////   video: "yes",
-    bkgImage: {
-      small: "youngarts.jpg",
-      medium: "youngarts.jpg",
-      large: "youngarts.jpg"
-      
+ 
+    
+        bkgImage: {
+      small: "grimeblue.png",
+      medium: "grimeblue.png",
+      large: "grimeblue.png"    
     },
+    
     body: `
  <strong>Outside the Box: Jason Moran, Jazz & Skateboarding</strong><br />JavaScript, WebGL, Projection Mapping<br />36' x 24'<br />Feb 6, 2016<br />YoungArts Campus<br /><br />Created a sound reactive projection mapping installation inspired by the Spike Jonze/Mark Gonzalez masterpiece, Video Days. Built a 3D version of the video in JavaScript and HTML5 to be projected on a building across the plaza, designed to respond to live Jazz performed by Jason Moran as he and his band improvised to skateboarders skating a halfpipe in the plaza of Miami's YoungArts campus.
     `
@@ -239,13 +298,17 @@ header: "",
     section: "Deauville Beach Resort. Miami Beach, FL",
 //////////////////////////     icon: "pcy.svg",
     header: "",
-    video: "yes",
-   bkgImage: {
-     wm: "assets/images/bkg/mygoodgood.webm",
-       mp: "assets/images/bkg/mygoodgood.mp4",
-      name:"7"
+  ////////  video: "yes",
+ 
+    
+        bkgImage: {
+      small: "grimeblue.png",
+      medium: "grimeblue.png",
+      large: "grimeblue.png"
+      
     },
-    body: `
+  
+  body: `
  <strong>My Good, Good Friend App</strong><br />Videography, Photography, Field Audio, JavaScript, WebGL, HTML5, WebVR<br />February 26, 2016 — February 27, 2016<br />Filmgate Interactive Creative Festival<br /><br />Sound-reactive 3D storyword and mobile VR experience designed for Miami transmedia festival, Filmgate.
 `
   },
@@ -261,12 +324,15 @@ header: "",
 //////////////////////////     icon: "pcy.svg",
     header: "",
   //////////////  video: "yes",
-    bkgImage: {
-      small: "omiamiphenomenal2.gif",
-      medium: "omiamiphenomenal2.gif",
-      large: "omiamiphenomenal2.gif"
+ 
+        bkgImage: {
+      small: "grimeblue.png",
+      medium: "grimeblue.png",
+      large: "grimeblue.png"
       
     },
+  
+  
     body: `
  <strong>InterContinental Miami Art Showcase</strong><br />Curation, Design, Development, JavaScript, WebGL, Video, Photography<br />18-story LEDs, hotel marquee, 12 interior screens<br />November 2015 - January 2016<br />InterContinental Hotel Miami<br /><br />Curated a year long series with 4 Miami New Media artists, including myself, to take over the interior and exterior of the downtown Miami property with site specific art.
 `
@@ -287,12 +353,15 @@ header: "",
 //////////////////     icon: "pcy.svg",
     header: "",
  //////////////////////////   video: "yes",
-    bkgImage: {
-      small: "6-IMG_0284_new-1060x707.jpg",
-      medium: "6-IMG_0284_new-1060x707.jpg",
-      large: "6-IMG_0284_new-1060x707.jpg"
+ 
+        bkgImage: {
+      small: "grimeblue.png",
+      medium: "grimeblue.png",
+      large: "grimeblue.png"
       
     },
+  
+  
     body: `
  <strong>Phenomenal Amarante</strong><br />Curation, Design, Development, JavaScript, WebGL<br />60'x20'<br />December 10, 2014<br />FIU-Wolfsonian Museum<br /><br />Sound Reactive projection mapping musical collaboration for official opening of Art Basel 2014. Organized the show, developed the browser app and executed the projector setup and mapping in collaboration with Ed Selego.
     `
@@ -312,12 +381,16 @@ header: "",
 //////////////////     icon: "pcy.svg",
     header: "",
  //////////////////////////   video: "yes",
-    bkgImage: {
-      small: "scimuseum.jpg",
-      medium: "scimuseum.jpg",
-      large: "scimuseum.jpg"
+ 
+    
+        bkgImage: {
+      small: "grimeblue.png",
+      medium: "grimeblue.png",
+      large: "grimeblue.png"
       
     },
+  
+  
     body: `
  <strong>Next – From Nano to Macro, Innovation at Every Scale</strong><br />JavaScript, WebGL<br />60'x20'<br />October 14, 2014<br />Patricia and Phillip Frost Museum of Science<br /><br />A real-life animated GIF visitors could step inside co-create together. The immersive, interactive experience was brought to life by the creative technology of projection mapping, motion sensing and beautiful content. Organized the show, developed the browser app and executed the projector setup and mapping in collaboration with Ed Selego.
     `
@@ -335,12 +408,15 @@ header: "",
 //////////////////     icon: "pcy.svg",
     header: "",
  //////////////////////////   video: "yes",
-    bkgImage: {
-      small: "interactiveexped.gif",
-      medium: "interactiveexped.gif",
-      large: "interactiveexped.gif"
+ 
+        bkgImage: {
+      small: "grimeblue.png",
+      medium: "grimeblue.png",
+      large: "grimeblue.png"
       
     },
+  
+  
     body: `
  <strong>Interactive Storytelling Expeditions</strong><br />Creative Direction, Art Direction, Concepting, Writing, Design, Illustration, JavaScript, HTML5, Audio<br />December 1, 2014<br />Web Based Project<br /><br />A Knight Foundation original report I created to demonstrate one approach to immersive storytelling for organizations and publications.
     `
