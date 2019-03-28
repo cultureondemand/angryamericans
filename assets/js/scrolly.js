16617ef8c95e9ab5f1ca93dab710c375c896ec70
Eric C Schoenborn <cultureondemand@gmail.com>
2019-03-27 20:54:28 -0400

    Update scrolly.js

--- assets/js/scrolly.js
+++ assets/js/scrolly.js
@@ -1,12 +1,16 @@
 var scrollVis=function(){var width=window.innerWidth,height=window.innerHeight;var margin={top:0,left:0,bottom:0,right:0};var lastIndex=-1;var activeIndex=0;var svg=null;var g=null;var activateFunctions=[];var updateFunctions=[];var chart=function(selection){selection.each(function(){setupSections();});};setupVis=function(){};setupSections=function(){activateFunctions[0]=showTitle0;activateFunctions[1]=showTitle1;activateFunctions[2]=showTitle2;activateFunctions[3]=showTitle3;activateFunctions[4]=showTitle4;activateFunctions[5]=showTitle5;activateFunctions[6]=showTitle6;activateFunctions[7]=showTitle7;activateFunctions[8]=showTitle8;activateFunctions[9]=showTitle9;activateFunctions[10]=showTitle10;activateFunctions[11]=showTitle11;activateFunctions[12]=showTitle12;activateFunctions[13]=showTitle13;activateFunctions[14]=showTitle14;activateFunctions[15]=showTitle15;activateFunctions[16]=showTitle16;activateFunctions[17]=showTitle17;activateFunctions[18]=showTitle18;activateFunctions[19]=showTitle19;activateFunctions[20]=showTitle20;activateFunctions[21]=showTitle21;activateFunctions[22]=showTitle22;activateFunctions[23]=showTitle23;for(var i=0;i<24+1;i++){updateFunctions[i]=function(){};}};
 
-function showTitle0(){var vis=document.getElementById("vis");vis.setAttribute("style","display:none; ");
-                      var outputA=document.getElementById("outputA");outputA.setAttribute("style","display:none; ");
+function showTitle0(){
+                      var vis=document.getElementById("vis");vis.setAttribute("style","display:none; ");
+      //////////////  var outputA=document.getElementById("outputA");outputA.setAttribute("style","display:none; ");
                       var logoOpener=document.getElementById("logoOpener");logoOpener.setAttribute("style","display:inline-block; ");
                       var openerimg=document.getElementById("openerimg");openerimg.setAttribute("style","visibility:visible; ");}
+
+                         
 function showTitle1(){
+  
 var scrollimg=document.getElementById("scrollimg");
-var vid1=document.getElementById("bgvid1");vid1.play();
+///////////////var vid1=document.getElementById("bgvid1");vid1.play();
 scrollimg.setAttribute("style","display:none; ");
 var logoOpener=document.getElementById("logoOpener");
 logoOpener.setAttribute("style","display:none; ");
@@ -23,8 +27,8 @@ openerimg.setAttribute("style","visibility:hidden; ");
                          
 }
 function showTitle2(){
-var vid1=document.getElementById("bgvid1");vid1.pause();
-var vid2=document.getElementById("bgvid1");vid2.pause();
+/////var vid1=document.getElementById("bgvid1");vid1.pause();
+//////var vid2=document.getElementById("bgvid1");vid2.pause();
  
   ///////var outputA=document.getElementById("outputA");
 ///////// outputA.setAttribute("style","display:none; ");
@@ -33,52 +37,52 @@ var vid2=document.getElementById("bgvid1");vid2.pause();
 ////outputB.setAttribute("style","display:fixed; ");
 }
 function showTitle3(){
-var vid2=document.getElementById("bgvid2");
-vid2.play();
+///////var vid2=document.getElementById("bgvid2");
+//////vid2.play();
 ///////  var outputB=document.getElementById("outputB");
 ///////// outputB.setAttribute("style","display:none; ");
 
 
 }
 function showTitle4(){
-var vid2=document.getElementById("bgvid2");
-vid2.pause();}
+//////var vid2=document.getElementById("bgvid2");
+///////vid2.pause();}
 function showTitle5(){
-  var vid3=document.getElementById("bgvid3");vid3.pause();
+ ///// var vid3=document.getElementById("bgvid3");vid3.pause();
 }
 function showTitle6(){
-  var vid3=document.getElementById("bgvid3");vid3.play();
+////  var vid3=document.getElementById("bgvid3");vid3.play();
 }
 function showTitle7(){
-  var vid3=document.getElementById("bgvid3");vid3.pause();
-var vid4=document.getElementById("bgvid4");vid4.play();
+////  var vid3=document.getElementById("bgvid3");vid3.pause();
+/////var vid4=document.getElementById("bgvid4");vid4.play();
 }
 function showTitle8(){
-var vid4=document.getElementById("bgvid4");vid4.play();
-var vid5=document.getElementById("bgvid5");vid5.pause();
+///var vid4=document.getElementById("bgvid4");vid4.play();
+////var vid5=document.getElementById("bgvid5");vid5.pause();
 }
 function showTitle9(){
-var vid4=document.getElementById("bgvid4");vid4.pause();
-var vid5=document.getElementById("bgvid5");vid5.play();
+////var vid4=document.getElementById("bgvid4");vid4.pause();
+/////var vid5=document.getElementById("bgvid5");vid5.play();
 }
 function showTitle10(){
-var vid5=document.getElementById("bgvid5");vid5.pause();
-var vid6=document.getElementById("bgvid6");vid6.pause();
+///var vid5=document.getElementById("bgvid5");vid5.pause();
+/////var vid6=document.getElementById("bgvid6");vid6.pause();
 }
 
 function showTitle11(){
-var vid6=document.getElementById("bgvid6");vid6.play();
+///var vid6=document.getElementById("bgvid6");vid6.play();
 }
 function showTitle12(){
-var vid6=document.getElementById("bgvid6");vid6.pause();
-var vid7=document.getElementById("bgvid7");vid7.pause();
+////var vid6=document.getElementById("bgvid6");vid6.pause();
+/////var vid7=document.getElementById("bgvid7");vid7.pause();
 }
 function showTitle13(){
-  var vid7=document.getElementById("bgvid7");vid7.play();
+ ///// var vid7=document.getElementById("bgvid7");vid7.play();
 /////////var vid8=document.getElementById("bgvid8");vid8.pause();
 }
 function showTitle14(){
-var vid7=document.getElementById("bgvid7");vid7.pause();
+//////var vid7=document.getElementById("bgvid7");vid7.pause();
 ///////var vid8=document.getElementById("bgvid8");vid8.play();
 }
 function showTitle15(){
